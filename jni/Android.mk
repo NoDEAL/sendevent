c7fbfbfc69b7a8d01e1c107b513b0e9da9dbe728
NoDEAL <kjiwhan0120@gmail.com>
2015-07-15 02:02:49 +0900

    EF63L is not standard any more

--- jni/Android.mk
+++ jni/Android.mk
@@ -13,7 +13,7 @@ include $(CLEAR_VARS)
 
 LOCAL_LDFLAGS   += -static
 LOCAL_CFLAGS    += -std=c99
-LOCAL_MODULE    := sendevent
-LOCAL_SRC_FILES := sendevent.c
+LOCAL_MODULE    := sendevent_ef63l
+LOCAL_SRC_FILES := sendevent_ef63l.c
 
 include $(BUILD_EXECUTABLE)
